13f3b5b7e6f831a2f1d719fac9e080ba24bd31a4
jbisne <jisun7894@gmail.com>
2019-06-10 22:35:23 +0900

    19-06-10 커밋

--- oracle savefile/Practice_TeamDB.sql
+++ oracle savefile/Practice_TeamDB.sql
@@ -4,19 +4,13 @@ select*from REPORT;
 select*from REVIEW;
 select*from REPLY;
 
-select * from GOODSBOARD;
 commit;
+select * from MEMBERS;
+select * from GOODSBOARD;
+select * from REPORT;
+select * from REVIEW;
+select * from REPLY;
 
-insert into GOODSBOARD values(
-    '01',0,'3000', '싸게사세요', 'MOMOLOVE', '설명설명','서울시 금천구 가산동',
-    'X좌표','Y좌표','image1','썸네일','image2','image3','NoImage','2019.06.06',
-    '0','도서','QR코드','완료',0,0,0,0
-);
-insert into GOODSBOARD values(
-    '02',0,'10000', '팝니당', 'SEULGILOVE', '설명설명','서울시 강서구 가양동',
-    'X좌표','Y좌표','image1','썸네일','image2','NoImage','NoImage','2019.06.01',
-    '0','자동차','QR코드','완료',0,0,0,0
-);
-
+delete from GOODSBOARD where BID='21';
 
 
