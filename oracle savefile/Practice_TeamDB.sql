9b883c12ea10ebaa24b8600c27739be1133660f0
jbisne <jisun7894@gmail.com>
2019-06-19 17:41:12 +0900

    2019.06.19 개념공부법 커밋

--- oracle savefile/Practice_TeamDB.sql
+++ oracle savefile/Practice_TeamDB.sql
@@ -4,6 +4,9 @@ select*from REPORT;
 select*from REVIEW;
 select*from REPLY;
 
+delete from members where MEMAIL='jisun9206@naver.com';
+delete from GOODSBOARD  where BID='55';
+update members set MPWCONFIRM='1234';
 commit;
 select * from MEMBERS;
 select * from GOODSBOARD;
@@ -12,3 +15,4 @@ select * from REVIEW;
 select * from REPLY;
 
 select * from user_list;
+drop table GOODSBOARD;
